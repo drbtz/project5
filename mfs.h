bfedd8518e67f2d758192505eeaddd31bb387c6d
Richard Barker <rcbarker2@wisc.edu>
2014-05-02 15:22:03 -0500

    Update Howto and add package struct

--- mfs.h
+++ mfs.h
@@ -17,6 +17,18 @@ typedef struct __MFS_DirEnt_t {
     int  inum;      // inode number of entry (-1 means entry not used)
 } MFS_DirEnt_t;
 
+typedef struct __Package_t {
+	int 		requestType;	//Request being sent to server(read, write, etc)
+	int 		pinum;			//The parent inode number
+	int 		inum;			//The inode number
+	MFS_Stat_t 	m;				//Used by MFS_Stat for formatting
+	char 		name[60];		//The file name
+	int 		block;			//The block of the inode
+	char 		buffer[4096];	//buffer for read request from client
+	int 		type;			//The type of the inode(file or directory)
+	int			result;			//The result of the request sent to server
+} Package_t;
+
 
 int MFS_Init(char *hostname, int port);
 int MFS_Lookup(int pinum, char *name);
