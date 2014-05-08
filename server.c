ac43db8b5dc412c7d5f77a4c0ebb53e28593a0b8
tony <ajbublitz@wisc.edu>
2014-05-08 14:25:35 -0500

    sync

--- server.c
+++ server.c
@@ -696,7 +696,7 @@ int server_shutdown()
 	fsync(imageFD);
 	close(imageFD);
 	buffer->result = 0;
-	UDP_Write(sd, &s, buffer, sizeof(Package_t));
+	UDP_Write(sd, &s, (char*)buffer, sizeof(Package_t));
 	exit(0);
 	return 0;
 }
@@ -828,11 +828,11 @@ main(int argc, char *argv[])
 
 	if(!DEBUG)
 	{
-		printf("                                SERVER:: waiting in loop\n");
+		//printf("                                SERVER:: waiting in loop\n");
 
 		while (1) {
 
-			int rc = UDP_Read(sd, &s, buffer, sizeof(Package_t));
+			int rc = UDP_Read(sd, &s, (char*)buffer, sizeof(Package_t));
 
 			if (rc > 0)
 			{
@@ -841,7 +841,7 @@ main(int argc, char *argv[])
 				//printf("                                SERVER:: read %d bytes (message: '%s')\n", rc, buffer->name);
 				//Package_t reply;
 				//strcpy(buffer->name, "Reply");
-				rc = UDP_Write(sd, &s, buffer, sizeof(Package_t));
+				rc = UDP_Write(sd, &s, (char*)buffer, sizeof(Package_t));
 			}
 		}
 	}
